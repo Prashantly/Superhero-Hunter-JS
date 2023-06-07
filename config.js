b6bcecb47166f9d83d8bac88b7c8e8504ddfb2a5
Prashant <prashantyallatti79968@gmail.com>
2023-06-07 15:42:06 +0530

    remove api keys from config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const PUBLIC_KEY = "bbf1d1662eb22d07c57a5a25064571f1";
-const HASH = "9c0af0622295e7724d2bf473fd8c6765";
+const PUBLIC_KEY = process.env.PUBLIC_KEY;
+const HASH = process.env.HASH;
 
 export { PUBLIC_KEY, HASH };
